9f37c8565a83fb8c23cdfb7f2481c783b4abfca6
João Alberto <hey@joaoalberto.dev>
2022-03-05 01:02:29 -0300

    chore: config hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomiclabs/hardhat-waffle");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.}`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.}`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY]
+    },
+  },
+  solidity: "0.8.4",
+};
