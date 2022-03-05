ed2069d769d87bf2ed8c418200f96dcb6573f4b8
João Alberto <hey@joaoalberto.dev>
2022-03-05 12:17:57 -0300

    chore: NFTMarketplace create function to fetch all unsold items

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -81,7 +81,7 @@ contract NFTMarketplace is ReentrancyGuard {
         );
     }
 
-    function createMarketplaceSale(address NFTContract, uint356 itemID)
+    function createMarketplaceSale(address NFTContract, uint256 itemID)
         public
         payable
         nonReentrant
@@ -100,5 +100,28 @@ contract NFTMarketplace is ReentrancyGuard {
 
         itemsSold.increment();
         payable(owner).transfer(listingPrice);
+    }
+
+    function fetchMarketplaceUnsoldItems()
+        public
+        view
+        returns (MarketplaceItem[] memory)
+    {
+        uint itemsCount = itemIDS.current();
+        uint unsoldItemsCount = itemIDS.current() - itemsSold.current();
+        uint currentIndex = 0;
+
+        MarketplaceItem[] memory items = new MarketplaceItem[](unsoldItemsCount);
+
+        for (uint i = 0; i < itemsCount; i++) {
+            if (IDMarketeplaceItem[i + 1].owner == address(0)) {
+                uint currentID = IDMarketeplaceItem[i + 1].itemID;
+                MarketplaceItem storage currentItem = IDMarketeplaceItem[currentID];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items
     }
 }
